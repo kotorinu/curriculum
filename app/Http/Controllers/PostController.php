65e53893059404fa0219abc07721f8e937d468b9
kotorinu <koto.tama.yellow@gmail.com>
2023-09-13 16:08:44 +0900

    記事投稿作成画面の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,18 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);  
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+    //getPaginateByLimit()はPost.phpで定義したメソッドです。
+    }
+
+    public function show(Post $post)
+    {
+       return view('posts.show')->with(['post' => $post]);  
        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+    }
+    
+    public function create()
+    {
+        return view('posts/create');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ use App\Http\Controllers\PostController;
    // return view('welcome');
 //});
 Route::get('/', [PostController::class, 'index']);
+ROUTE::get('/posts/create',[PostController::class,'create']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
